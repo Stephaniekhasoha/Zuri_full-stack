db045cbabbc578c82e31a1fcbc6066576e01200b
Stephaniekhasoha <khasohastephanie@gmail.com>
2022-05-27 21:45:37 +0300

    my zuri tasks

---
+++ word_count.py
@@ -0,0 +1,10 @@
+sentence = "My first python program is fun"
+
+#original string
+print ("Our sentence for counting: " + sentence)
+
+# using split() function
+num = len(sentence.split())
+
+# total number of words
+print("Your word count is: " + str(num))
